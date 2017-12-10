8a6c219984ad4422fc75d6762d0133f927466c24
Nathan Robbert <admin@bigrocketgames.com>
2017-12-10 13:55:43 -0800

    feat: Created a component that will be used to add a new question to a deck.

---
+++ components/AddQuestion.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+
+class AddQuestion extends Component {
+  render() {
+    return (
+      <View>
+        <Text>
+          Render a form to add a question to a deck.
+        </Text>
+      </View>
+    )
+  }
+}
+
+export default AddQuestion
