e7b2270318a38719c61a6d97e65726e64dd0b1dc
Nathan Robbert <admin@bigrocketgames.com>
2017-12-10 13:55:23 -0800

    feat: Created component that will be used to create a new deck.

---
+++ components/AddDeck.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+
+class AddDeck extends Component {
+  render() {
+    return (
+      <Text>Add a deck here</Text>
+    )
+  }
+}
+
+export default AddDeck
