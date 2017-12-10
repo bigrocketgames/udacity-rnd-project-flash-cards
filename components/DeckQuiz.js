a73667f2da210600ba9c4e958fbc683046cc49b4
Nathan Robbert <admin@bigrocketgames.com>
2017-12-10 13:57:50 -0800

    feat: Created a DeckQuiz component that will be used for displaying the quiz for a particular deck.

---
+++ components/DeckQuiz.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+class DeckQuiz extends Component {
+  render() {
+    return(
+      <View>
+        <Text>
+          Have quiz questions show in order as passed to state.
+
+          Multiple choice, true/false, or fill in the blank?
+
+          button for submitting answer.
+
+          Keep track of score and display it at the top-right of the screen.
+
+          After final question, show a results screen.
+        </Text>
+      </View>
+    )
+  }
+}
