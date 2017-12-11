3beaf537c76cbd450a963e5c6cf147be2f27a284
Nathan Robbert <admin@bigrocketgames.com>
2017-12-10 21:09:46 -0800

    fix:  Added the keyextractor to get rid of warning and modified the way the deck was sent to the rendering component for each deck, to render the proper items.

--- components/Decks.js
+++ components/Decks.js
@@ -44,20 +44,20 @@ class Decks extends Component {
   render() {
     const { decks } = this.state
 
-    console.log(decks)
-
     return (
       <View>
         <Text style={{fontSize: 40, marginTop: 10, alignSelf: 'center'}}>Choose a Deck</Text>
         { decks ? 
           <FlatList 
+            style={styles.list}
             data={decks}
-            renderItem={({deck}) => <TouchableOpacity key={deck.title} onPress={() => this.props.navigation.navigate(
+            renderItem={(deck) => <TouchableOpacity onPress={() => this.props.navigation.navigate(
               'DeckDetail',
               { deck: deck }
             )}>
-              <DeckHomeCard deck={deck} />
+              <DeckHomeCard deck={deck.item} />
             </TouchableOpacity>}
+            keyExtractor={(deck) => deck.title}
           /> 
           : 
           'Please add your first deck of cards to begin.'}
@@ -65,5 +65,12 @@ class Decks extends Component {
     )
   }
 }
+
+const styles = StyleSheet.create({
+  list: {
+    padding: 15,
+    padding: 15
+  }
+})
 
 export default Decks
