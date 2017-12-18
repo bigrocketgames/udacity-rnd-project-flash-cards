c170a9478bd3add67c18663ba1d0ae22f4e7cf5e
Nathan Robbert <admin@bigrocketgames.com>
2017-12-17 20:54:59 -0800

    feat:  Beginning to build the basics of the AddQuestion component.

--- components/AddQuestion.js
+++ components/AddQuestion.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TextInput, KeyboardAvoidingView } from 'react-native'
 
 class AddQuestion extends Component {
+  state = {
+    question: '',
+    answer: ''
+  }
+
   render() {
+    const { question, answer } = this.state
+    console.log(this.props)
+
     return (
       <View>
         <Text>
-          Render a form to add a question to a deck.
+          Add a new question and answer to the {this.props.navigation.state.params.deckId} deck.
         </Text>
+        <KeyboardAvoidingView>
+          <TextInput multiline={true} value={question}></TextInput>
+          <TextInput multiline={true} value={answer}></TextInput>
+        </KeyboardAvoidingView>
       </View>
     )
   }
