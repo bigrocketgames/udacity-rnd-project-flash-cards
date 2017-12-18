ce519d14b24dfd9924301cfc3a2d3ef3d12b74c5
Nathan Robbert <admin@bigrocketgames.com>
2017-12-17 20:54:16 -0800

    chore: Adding style to the DeckDetail component.

--- components/DeckDetail.js
+++ components/DeckDetail.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 import { connect } from 'react-redux'
 
+import { red } from '../utils/colors'
+
 class DeckDetail extends Component {
   static navigationOptions = ({ navigation }) => {
     return {
@@ -13,23 +15,64 @@ class DeckDetail extends Component {
     const { deck } = this.props
 
     return (
-      <View>
-        <Text>{deck.title}</Text>
-        <Text>{deck.questions.length > 0 ? deck.questions.length : 0} questions </Text>
-        <TouchableOpacity onPress={() => this.props.navigation.navigate(
-              'AddQuestion',
-              { deckId: deck.title }
-            )}>
-        <Text>Add A Card</Text>
-        </TouchableOpacity>
-        {deck.questions.length > 0 ? <TouchableOpacity onPress={() => this.props.navigation.navigate(
-              'DeckQuiz',
-              { deckId: deck.title }
-            )}><Text>Start Quiz</Text></TouchableOpacity> : null}
+      <View style={styles.deckContainer}>
+        <View style={{alignItems: 'center', margin: 10, padding: 20}}>
+          <Text style={styles.deckTitle}>{deck.title}</Text>
+          <Text style={styles.deckCardCount}>{deck.questions.length > 0 ? deck.questions.length : 0} questions </Text>
+          <TouchableOpacity style={styles.submitbtn} onPress={() => this.props.navigation.navigate(
+                'AddQuestion',
+                { deckId: deck.title }
+              )}>
+          <Text>Add A Card</Text>
+          </TouchableOpacity>
+          {deck.questions.length > 0 ? <TouchableOpacity style={styles.submitbtn} onPress={() => this.props.navigation.navigate(
+                'DeckQuiz',
+                { deckId: deck.title }
+              )}><Text>Start Quiz</Text></TouchableOpacity> : null}
+        </View>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  deckContainer: {
+    padding: 20,
+    marginLeft: 15,
+    marginRight: 15,
+    marginTop: 10,
+    justifyContent: 'center',
+    borderRadius: 10,
+    shadowRadius: 3,
+    shadowOpacity: 0.8,
+    shadowColor: 'rgba(0,0,0,0.4)',
+    shadowOffset: {
+      width: 0,
+      height: 3
+    }
+  },
+  deckTitle: {
+    flexDirection: 'row',
+    fontSize: 30,
+    margin: 5
+  },
+  deckCardCount: {
+    flexDirection: 'row',
+    fontSize: 20,
+    padding: 10
+  },
+  submitbtn: {
+    backgroundColor: red,
+    borderRadius: 7,
+    padding: 10,
+    height: 45,
+    marginTop: 15,
+    marginRight: 40,
+    marginLeft: 40,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
 
 const mapStateToProps = (state, {navigation}) => {
   const { deckId } = navigation.state.params
