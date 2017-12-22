f49e7b538e7e25b4f737728bd5b94061a58e2ff4
Nathan Robbert <admin@bigrocketgames.com>
2017-12-21 21:18:17 -0800

    feat: Quiz is working with results showing at end.  Need to animate flip to show answer and show the number of remaining cards in the quiz.

--- components/DeckQuiz.js
+++ components/DeckQuiz.js
@@ -5,43 +5,79 @@ class DeckQuiz extends Component {
   state = {
     questionNumber: 1,
     correct: 0,
-    showAnswer: false
+    showAnswer: false, 
+    showResults: false
   }
 
   handleCorrect = () => {
-    console.log(this.state)
+    if (this.state.questionNumber < this.props.navigation.state.params.deck.questions.length) {
+      const questionNumber = this.state.questionNumber + 1
+      const correct = this.state.correct + 1
+  
+      this.setState(() => ({
+        questionNumber,
+        correct
+      }))
+    } else {
+      const correct = this.state.correct + 1
+
+      this.setState(() => ({
+        correct, 
+        showResults: true
+      }))
+    }
   }
 
   handleIncorrect = () => {
-    console.log(this.state)
+    if (this.state.questionNumber < this.props.navigation.state.params.deck.questions.length) {
+      const questionNumber = this.state.questionNumber + 1
+  
+      this.setState(() => ({
+        questionNumber
+      }))
+    } else {
+
+      this.setState(() => ({
+        showResults: true
+      }))
+    }
   }
 
   render() {
-    const { questionNumber, correct, showAnswer } = this.state
+    const { questionNumber, correct, showAnswer, showResults } = this.state
     const { deck } = this.props.navigation.state.params
 
-    if (showAnswer) {
-      return(
+    if (showResults) {
+      return (
         <View>
           <Text>
-            {deck.questions[questionNumber - 1].answer}
+            These will be the results. {Math.round((correct/questionNumber)*100)}%
           </Text>
-          <TouchableOpacity onPress={() => this.handleCorrect()}><Text>CORRECT</Text></TouchableOpacity>
-          <TouchableOpacity onPress={() => this.handleIncorrect()}><Text>INCORRECT</Text></TouchableOpacity>
         </View>
       )
     } else {
-      return(
-        <View>
-          <Text>
-            {deck.questions[questionNumber - 1].question}
-          </Text>
-          <TouchableOpacity onPress={() => this.handleCorrect()}><Text>CORRECT</Text></TouchableOpacity>
-          <TouchableOpacity onPress={() => this.handleIncorrect()}><Text>INCORRECT</Text></TouchableOpacity>
-        </View>
-      )
+      if (showAnswer) {
+        return(
+          <View>
+            <Text>
+              {deck.questions[questionNumber - 1].answer}
+            </Text>
+            <TouchableOpacity onPress={() => this.handleCorrect()}><Text>CORRECT</Text></TouchableOpacity>
+            <TouchableOpacity onPress={() => this.handleIncorrect()}><Text>INCORRECT</Text></TouchableOpacity>
+          </View>
+        )
+      } else {
+        return(
+          <View>
+            <Text>
+              {deck.questions[questionNumber - 1].question}
+            </Text>
+            <TouchableOpacity onPress={() => this.handleCorrect()}><Text>CORRECT</Text></TouchableOpacity>
+            <TouchableOpacity onPress={() => this.handleIncorrect()}><Text>INCORRECT</Text></TouchableOpacity>
+          </View>
+        )
+      }
     }
-    
   }
 }
 
